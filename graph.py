c68d04a5f73977e1e437a6caf3bf2a30f982f2f5
IrisxLiu <xhliu926@gmail.com>
2023-11-22 10:19:12 -0800

    Updated graph and vertex

--- graph.py
+++ graph.py
@@ -1,46 +1,60 @@
 """ DRAFT: need to revise!!!
+    class Graph
+        Attributes:
+            rows: total rows and columns in the graph
+            vert_list: list of all vertices in the graph
+            vert_count: number of vertices in the graph
+
+        Methods:
+            draw_graph: create vertices to fill in the graph, MAZE ALGO USE THIS
+                Parameters:
+                    rows: number of rows and columns in total in the graph
+                    width: width of the grid
 """
-
-
-import pygame
 from vertex import Vertex
 
 
-# Initialize the grid
-WIDTH = 1000
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("Pathfinding Odyssey")
-
-
 class Graph:
-    def __init__(self):
-        self.vertList = {}
-        self.numVertices = 0
+    def __init__(self, rows):
+        self.rows = rows
+        self.vert_list = []
+        self.vert_count = 0
 
-    def addVertex(self,key):
-        self.numVertices = self.numVertices + 1
-        newVertex = Vertex(key)
-        self.vertList[key] = newVertex
-        return newVertex
+    def draw_graph(self, rows, width):
+        graph = self.vert_list
+        sub_width = width // rows
+        for row in range(rows):
+            graph.append([])
+            for column in range(rows):
+                graph[row].append(Vertex(row, column, sub_width))
+                self.vert_count += 1
+        return graph
+    
+    
+    # def add_vertex(self, key):
+    #     self.vert_count = self.numVertices + 1
+    #     vertex = Vertex(key)
+    #     self.vert_list[key] = vertex
+    #     return vertex
 
-    def getVertex(self,n):
-        if n in self.vertList:
-            return self.vertList[n]
-        else:
-            return None
+    # def get_vertex(self, vertex):
+    #     if vertex in self.vert_list:
+    #         return self.vert_list[vertex]
+    #     else:
+    #         return None
 
-    def __contains__(self,n):
-        return n in self.vertList
+    # def __contains__(self, vertex):
+    #     return vertex in self.vert_list
 
-    def addEdge(self,f,t,weight=0):
-        if f not in self.vertList:
-            nv = self.addVertex(f)
-        if t not in self.vertList:
-            nv = self.addVertex(t)
-        self.vertList[f].addNeighbor(self.vertList[t], weight)
+    # def add_edge(self, f, t, weight=0):
+    #     if f not in self.vert_list:
+    #         nv = self.add_vertex(f)
+    #     if t not in self.vert_list:
+    #         nv = self.add_vertex(t)
+    #     self.vert_list[f].add_neighbor(self.vert_list[t], weight)
 
-    def getVertices(self):
-        return self.vertList.keys()
+    # def get_vertices(self):
+    #     return self.vert_list.keys()
 
-    def __iter__(self):
-        return iter(self.vertList.values())
+    # def __iter__(self):
+    #     return iter(self.vert_list.values())
--- vertex.py
+++ vertex.py
@@ -1,52 +1,57 @@
 """ class Vertex
-    Attributes:
-        id: id of the vertex
-        neighbors: list of neighbors of the vertex
-        x: position of row
-        y: position of colomn
-        color: color/status of the vertex, default to white
-        width: width of the vertex
+        Attributes:
+            row: position of row
+            column: position of colomn
+            color: color/status of the vertex, default to white
+            width: width of the vertex, also is the weight of edges
+            neighbors: list of non-wall neighbors of the vertex
 
-    Methods:
-        get_pos: get position (x, y)
-        is_open, is_closed, is_barrier, is_start, is_end, is_path: return True if the vertex is of the status
-        make_open, make_closed, make_barrier, make_start, make_end, make_path: change the status
-        reset_grid: change vertex to the default status
+        Methods:
+            get_position: return the position, represented by (row, column)
+            is_open, is_closed, is_barrier, is_start, is_end, is_path: return True if the vertex is of the status
+            make_open, make_closed, make_barrier, make_start, make_end, make_path: change the status
+            reset_grid: change vertex to the default status
+            draw: draw the vertex on the grid
+                Parameters: current window
+            add_neighbor: add 4 adjacent vertices as neighbors if they are not walls
+                Parameters: grid
 """
+import pygame
+from graph import Graph
+
+WIDTH = 1000
+WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("Pathfinding Odyssey")
 
 
-# Colors as flag
+# Colors as status of vertices
 WHITE = (255, 255, 255) # default
-RED = (255, 0, 0) # closed/visited
-GREEN = (0, 255, 0) # open: it may not be used
+BLACK = (0, 0, 0) # wall
+RED = (255, 0, 0) # visited
+GREEN = (0, 255, 0) # open
+YELLOW = (255, 255, 0) # path
 ORANGE = (255, 165 ,0) # start
 BLUE = (0, 255, 0) # end
-BLACK = (0, 0, 0) # barrier
-YELLOW = (255, 255, 0) # path
 
 
 class Vertex:
-    def __init__(self, key, row, col, width, total_rows):
-        self.id = key
-        self.neighbors = []
-        self.x = row * width
-        self.y = col * width
-        self.color = WHITE
+    def __init__(self, row, column, width, total_rows, color=WHITE):
+        self.row = row
+        self.column = column
+        self.color = color
         self.width = width
         self.total_rows = total_rows
-        self.prev_nbr = None # does Djikstra need this?
-
-    def get_pos(self):
-        return (self.row, self.col)
+        self.neighbors = []
+        self.prev_nbr = None
+    
+    def is_wall(self):
+        return self.color == BLACK
     
     def is_open(self):
         return self.color == GREEN
     
-    def is_closed(self):
+    def is_visited(self):
         return self.color == RED
-    
-    def is_barrier(self):
-        return self.color == BLACK
     
     def is_start(self):
         return self.color == ORANGE
@@ -56,38 +61,85 @@ class Vertex:
     
     def is_path(self):
         return self.color == YELLOW
+    
+    # def is_prev_nbr(self, vertex):
+    #     return vertex == self.prev_nbr
 
-    def make_start(self):
+    def set_start(self):
         self.color = ORANGE
 
-    def make_end(self):
+    def set_end(self):
         self.color = BLUE
 
-    def make_open(self):
+    def set_open(self):
         self.color = GREEN
     
-    def make_closed(self):
+    def set_visted(self):
         self.color = RED
 
-    def make_barrier(self):
+    def set_wall(self):
         self.color = BLACK
 
-    def make_path(self):
+    def set_path(self):
         self.color = YELLOW
 
-    def reset_grid(self):
+    # def set_prev_nbr(self, vertex):
+    #     self.prev_nbr = vertex
+
+    def reset_vertex(self):
         self.color = WHITE
 
-    def add_neighbor(self,neighbor):
-        self.neighbors.append(neighbor)
+    def draw_vertex(self, window):
+        # pygame draw rectangle/vertex at position (x, y) of width and height
+        x = self.row * self.width
+        y = self.column * self.width
+        pygame.draw.rect(window, self.color, (x, y, self.width, self.width))
 
-    def __str__(self):
-        return str(self.id) + 'connects to: ' + str([x.id for x in self.neighbors])
+    # need to set the grid first, only add if it's not wall
+    def add_neighbor(self, graph):
+        self.neighbors = []
+        row = self.row
+        column = self.column
+        total = self.total_rows
+        # add vertex below the current vertex
+        if row < total - 1 and not graph[row + 1][column].is_wall():
+            self.neighbors.append(graph[row + 1][column])
+        # add vertex on top of the current vertex
+        if self.row > 0 and not graph[row - 1][column].is_wall():
+            self.neighbors.append(graph[row - 1][column])
+        # add vertex on the left
+        if column > 0 and not graph[row][column - 1].is_wall():
+            self.neighbors.append(graph[row][column - 1])
+        # add vertex on the right
+        if column < total - 1 and not graph[row][column + 1].is_wall():
+            self.neighbors.append(graph[row][column + 1])
+
+    # def get_position(self):
+    #     return (self.row, self.column)
     
-    """ to do
-        check if add_neighbors() is correct
-        def get_neighbors(self):
-        def make_prev_nbr(self, nbr):
-            self.prev_nbr = nbr
-        def get_prev_nbr(self):
-    """
+    def get_neighbors(self):
+        return [nbr for nbr in self.neighbors]
+    
+    # def __str__(self):
+    #     return str(self.id) + 'connects to: ' + str([nbr for nbr in self.neighbors])
+    
+    # def __lt__(self, other_vert):
+    #     return False
+
+
+# Create vertices to fill in the grid, in form of 2D List [[][][]...]
+def set_grid(total_rows, width):
+    grid = []
+    sub_width = width // total_rows
+    for row in range(total_rows):
+        grid.append([])
+        for col in range(total_rows):
+            grid[row].append(Vertex(row, col, sub_width, total_rows))
+    return grid
+
+# TODO
+# total rows不是vertex的attribute，应该写在graph里
+# set grid不是vertex的功能，应该写在graph里
+# 整理一下attribute和功能，分开写
+# vertex：自己的行、列、状态、nbr，功能：判断&调整状态，增加nbr，记录前一个nbr
+# graph：一共多少行、列，有哪些vertex，功能：画出整个graph
