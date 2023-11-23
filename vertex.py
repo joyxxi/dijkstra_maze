c7573741b46e67af513a5abe8a9f7532e3660d9e
IrisxLiu <144653224+IrisxLiu@users.noreply.github.com>
2023-11-23 00:32:13 -0800

    Updated vertex.py, waiting for maze generation to confirm

--- vertex.py
+++ vertex.py
@@ -1,27 +1,24 @@
 """ class Vertex
         Attributes:
             row: position of row
-            column: position of colomn
+            column: position of column
             color: color/status of the vertex, default to white
             width: width of the vertex, also is the weight of edges
-            neighbors: list of non-wall neighbors of the vertex
+            walls: walls in direction top, bottom, left, right, True means wall, False means not wall
+            visited: for generating maze, to track if the vertex is visited
+            neighbors: list of neighbors of the vertex, only add to the list when there's no wall between
 
         Methods:
-            get_position: return the position, represented by (row, column)
-            is_open, is_closed, is_barrier, is_start, is_end, is_path: return True if the vertex is of the status
-            make_open, make_closed, make_barrier, make_start, make_end, make_path: change the status
-            reset_grid: change vertex to the default status
-            draw: draw the vertex on the grid
+            is_open, is_closed, is_barrier, is_start, is_end, is_path: for dijkstra, return True if the vertex is of the status
+            make_open, make_closed, make_barrier, make_start, make_end, make_path: for dijkstra, change the status
+            reset_grid: just in case need to change vertex to the default status
+            add_neighbor: for dijkstra, and maze(?) add 4 adjacent vertices as neighbors if there are no wall between
+                Parameters: need to draw_graph() to get the vert_list to pass in as parameter
+            draw_wall: for visualization, draw wall in the four direction if wall is marked True
                 Parameters: current window
-            add_neighbor: add 4 adjacent vertices as neighbors if they are not walls
-                Parameters: grid
 """
 import pygame
 from graph import Graph
-
-WIDTH = 1000
-WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("Pathfinding Odyssey")
 
 
 # Colors as status of vertices
@@ -35,15 +32,15 @@ BLUE = (0, 255, 0) # end
 
 
 class Vertex:
-    def __init__(self, row, column, width, total_rows, color=WHITE):
+    def __init__(self, row, column, width, color=WHITE):
         self.row = row
         self.column = column
         self.color = color
         self.width = width
-        self.total_rows = total_rows
+        self.walls = {'top': True, 'bottom': True, 'left': True, 'right': True}
+        self.visited = False # for maze generation, flag True after being visited, to avoid using Set
         self.neighbors = []
-        self.prev_nbr = None
-    
+        
     def is_wall(self):
         return self.color == BLACK
     
@@ -61,9 +58,6 @@ class Vertex:
     
     def is_path(self):
         return self.color == YELLOW
-    
-    # def is_prev_nbr(self, vertex):
-    #     return vertex == self.prev_nbr
 
     def set_start(self):
         self.color = ORANGE
@@ -83,63 +77,64 @@ class Vertex:
     def set_path(self):
         self.color = YELLOW
 
-    # def set_prev_nbr(self, vertex):
-    #     self.prev_nbr = vertex
-
     def reset_vertex(self):
         self.color = WHITE
 
-    def draw_vertex(self, window):
-        # pygame draw rectangle/vertex at position (x, y) of width and height
-        x = self.row * self.width
-        y = self.column * self.width
-        pygame.draw.rect(window, self.color, (x, y, self.width, self.width))
-
-    # need to set the grid first, only add if it's not wall
-    def add_neighbor(self, graph):
+    # call this after the maze has been generated to update neighbors list
+    def add_neighbors(self, vert_list):
         self.neighbors = []
         row = self.row
         column = self.column
-        total = self.total_rows
+        total = len(vert_list)
+        vertex = vert_list[row][column]
         # add vertex below the current vertex
-        if row < total - 1 and not graph[row + 1][column].is_wall():
-            self.neighbors.append(graph[row + 1][column])
+        if row < total - 1 and not vertex.walls['bottom']:
+            self.neighbors.append(vert_list[row + 1][column])
         # add vertex on top of the current vertex
-        if self.row > 0 and not graph[row - 1][column].is_wall():
-            self.neighbors.append(graph[row - 1][column])
+        if self.row > 0 and not vertex.walls['top']:
+            self.neighbors.append(vert_list[row - 1][column])
         # add vertex on the left
-        if column > 0 and not graph[row][column - 1].is_wall():
-            self.neighbors.append(graph[row][column - 1])
+        if column > 0 and not vertex.walls['left']:
+            self.neighbors.append(vert_list[row][column - 1])
         # add vertex on the right
-        if column < total - 1 and not graph[row][column + 1].is_wall():
-            self.neighbors.append(graph[row][column + 1])
+        if column < total - 1 and not vertex.walls['right']:
+            self.neighbors.append(vert_list[row][column + 1])
 
-    # def get_position(self):
-    #     return (self.row, self.column)
-    
     def get_neighbors(self):
         return [nbr for nbr in self.neighbors]
     
-    # def __str__(self):
-    #     return str(self.id) + 'connects to: ' + str([nbr for nbr in self.neighbors])
-    
-    # def __lt__(self, other_vert):
-    #     return False
+    # call this when maze has been generated to draw wall if there's any
+    def draw_wall(self, window):
+        # pygame draw line from one position to another
+        width = self.width
+        x = self.row * width
+        y = self.column * width
+        # draw top wall
+        if self.walls['top']:
+            pygame.draw.line(window, BLACK, (x, y), (x + width, y))
+        # draw right wall
+        if self.walls['right']:
+            pygame.draw.line(window, BLACK, (x + width, y), (x + width, y + width))
+        # draw bottom wall
+        if self.walls['bottom']:
+            pygame.draw.line(window, BLACK, (x + width, y + width), (x, y + width))
+        # draw left wall
+        if self.walls['left']:
+            pygame.draw.line(window, BLACK, (x, y + width), (x, y))
 
 
-# Create vertices to fill in the grid, in form of 2D List [[][][]...]
-def set_grid(total_rows, width):
-    grid = []
-    sub_width = width // total_rows
-    for row in range(total_rows):
-        grid.append([])
-        for col in range(total_rows):
-            grid[row].append(Vertex(row, col, sub_width, total_rows))
-    return grid
+# DO NOT NEED THESE, SAVE HERE IN CASE!
+# def __str__(self):
+#     return str(self.id) + 'connects to: ' + str([nbr for nbr in self.neighbors])
 
-# TODO
-# total rows不是vertex的attribute，应该写在graph里
-# set grid不是vertex的功能，应该写在graph里
-# 整理一下attribute和功能，分开写
-# vertex：自己的行、列、状态、nbr，功能：判断&调整状态，增加nbr，记录前一个nbr
-# graph：一共多少行、列，有哪些vertex，功能：画出整个graph
+# def __lt__(self, other_vert):
+#     return False
+
+# def get_position(self):
+#     return (self.row, self.column)
+
+# def set_prev_nbr(self, vertex):
+#     self.prev_nbr = vertex
+
+# def is_prev_nbr(self, vertex):
+#     return vertex == self.prev_nbr
