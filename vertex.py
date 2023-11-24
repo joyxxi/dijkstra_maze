355648ec0bf395503c0a1ba99b67ba6a3e91ee90
IrisxLiu <144653224+IrisxLiu@users.noreply.github.com>
2023-11-23 16:37:32 -0800

    Merged graph into vertex.py

--- vertex.py
+++ vertex.py
@@ -10,16 +10,23 @@
 
         Methods:
             is_open, is_closed, is_barrier, is_start, is_end, is_path: for dijkstra, return True if the vertex is of the status
+            
             make_open, make_closed, make_barrier, make_start, make_end, make_path: for dijkstra, change the status
+            
             reset_grid: just in case need to change vertex to the default status
-            add_neighbor: for dijkstra, and maze(?) add 4 adjacent vertices as neighbors if there are no wall between
+            
+            add_neighbor: for dijkstra and maze, add 4 adjacent vertices as neighbors if there are no wall between
                 Parameters: need to draw_graph() to get the vert_list to pass in as parameter
+            
             draw_wall: for visualization, draw wall in the four direction if wall is marked True
                 Parameters: current window
+
+        Function:
+            draw_graph: create a 2D List to hold vertices
+                Parameters:
+                    rows: number of rows
+                    width: width of each row
 """
-import pygame
-
-
 # Colors as status of vertices
 WHITE = (255, 255, 255) # default
 BLACK = (0, 0, 0) # wall
@@ -46,7 +53,6 @@ class Vertex:
     def is_open(self):
         return self.color == GREEN
     
-    # Work for Dijkstra part
     def is_visited(self):
         return self.color == RED
     
@@ -68,7 +74,6 @@ class Vertex:
     def set_open(self):
         self.color = GREEN
     
-    # Work for Dijkstra part
     def set_visted(self):
         self.color = RED
 
@@ -103,39 +108,33 @@ class Vertex:
 
     def get_neighbors(self):
         return [nbr for nbr in self.neighbors]
-    
-    # # call this when maze has been generated to draw wall if there's any
-    # def draw_wall(self, window):
-    #     # pygame draw line from one position to another
-    #     width = self.width
-    #     x = self.row * width
-    #     y = self.column * width
-    #     # draw top wall
-    #     if self.walls['top']:
-    #         pygame.draw.line(window, BLACK, (x, y), (x + width, y))
-    #     # draw right wall
-    #     if self.walls['right']:
-    #         pygame.draw.line(window, BLACK, (x + width, y), (x + width, y + width))
-    #     # draw bottom wall
-    #     if self.walls['bottom']:
-    #         pygame.draw.line(window, BLACK, (x + width, y + width), (x, y + width))
-    #     # draw left wall
-    #     if self.walls['left']:
-    #         pygame.draw.line(window, BLACK, (x, y + width), (x, y))
 
 
-# DO NOT NEED THESE, SAVE HERE IN CASE!
-# def __str__(self):
-#     return str(self.id) + 'connects to: ' + str([nbr for nbr in self.neighbors])
+def draw_graph(rows, width):
+        graph = []
+        sub_width = width // rows
+        for row in range(rows):
+            graph.append([])
+            for column in range(rows):
+                graph[row].append(Vertex(row, column, sub_width))
+        return graph
 
-# def __lt__(self, other_vert):
-#     return False
 
-# def get_position(self):
-#     return (self.row, self.column)
-
-# def set_prev_nbr(self, vertex):
-#     self.prev_nbr = vertex
-
-# def is_prev_nbr(self, vertex):
-#     return vertex == self.prev_nbr
+# MOVED to main(), call this when maze has been generated to draw wall if there's any
+# def draw_wall(self, window):
+#     # pygame draw line from one position to another
+#     width = self.width
+#     x = self.row * width
+#     y = self.column * width
+#     # draw top wall
+#     if self.walls['top']:
+#         pygame.draw.line(window, BLACK, (x, y), (x + width, y))
+#     # draw right wall
+#     if self.walls['right']:
+#         pygame.draw.line(window, BLACK, (x + width, y), (x + width, y + width))
+#     # draw bottom wall
+#     if self.walls['bottom']:
+#         pygame.draw.line(window, BLACK, (x + width, y + width), (x, y + width))
+#     # draw left wall
+#     if self.walls['left']:
+#         pygame.draw.line(window, BLACK, (x, y + width), (x, y))
